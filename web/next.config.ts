d0a7ec915254a8209f1e42b7f5d8a54d8066ea44
Your Name <your@email.com>
2026-01-19 23:22:55 +0530

    next config edited

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,10 +1,10 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//     devIndicators: {
-//         appIsrStatus: false,
-//         buildActivity: false,
-//     },
-// };
+const nextConfig: NextConfig = {
+    // devIndicators: {
+    //     appIsrStatus: false,
+    //     buildActivity: false,
+    // },
+};
 
-// export default nextConfig;
+export default nextConfig;
